39968116f9cab3d2aa3a952602abfd594d7a26ad
李浩成 <506369534@qq.com>
2020-01-19 10:07:37 +0800

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,10 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'max-len':'off'
+    'max-len': 'off',
+    "prettier/prettier": ["error", {
+     "endOfLine": "auto"
+   }],
   },
   parserOptions: {
     parser: 'babel-eslint',
