d50a421a2d54ae9dc653cc77c4fcad554e495a8e
oagsate <506369534@qq.com>
2022-09-15 22:45:03 +0800

    fix: hide loading when register failed

--- src/app/routes/register/register.component.ts
+++ src/app/routes/register/register.component.ts
@@ -52,10 +52,15 @@ export class RegisterComponent {
 
   register() {
     this.loading = true;
-    this.userSvc.register(this.form.value).subscribe((res) => {
-      this.msgSvc.success('注册成功');
-      this.router.navigateByUrl('plaza');
-    });
+    this.userSvc.register(this.form.value).subscribe(
+      (res) => {
+        this.msgSvc.success('注册成功');
+        this.router.navigateByUrl('plaza');
+      },
+      () => {
+        this.loading = false;
+      }
+    );
   }
 
   onLoginClick() {
